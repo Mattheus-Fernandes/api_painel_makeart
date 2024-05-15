3d7a035bf8de2fca437de58adda3742ba594a5e0
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-05-15 16:49:32 -0300

    change

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const routes = require("./routes/route")
-const port = 3030
+const port = process.env.PORT || 3030
 
 
 app.use(cors())
